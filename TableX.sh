64c340ea2cd86de1ecb349cdd1450908076978fd
markbirss <markbirss@gmail.com>
2018-08-26 00:03:34 +0200

    Translated to English

--- TableX.sh
+++ TableX.sh
@@ -3,17 +3,17 @@
 ################################    Dependencias    #########################
 
 clear
-echo " Bienvenid@s al script de creacion"
+echo " Welcome to the creation script"
 sleep 1
-echo " de la imagen linux para una tablet allwinner "
+echo " of the linux image for an allwinner tablet "
 sleep 1
-echo " Instalando dependencias"
+echo " Installing dependancies"
 sleep 1
 #apt-get update
 apt-get install -y flex bison gcc-arm-linux-gnueabihf wget bc tree git debootstrap qemu-system-arm qemu-user-static build-essential libssl-dev libusb-1.0-0-dev bin86 libqt4-dev libncurses5 libncurses5-dev qt4-dev-tools u-boot-tools device-tree-compiler swig libpython-dev libqt4-dev libusb-dev zlib1g-dev pkg-config libgtk2.0-dev libglib2.0-dev libglade2-dev
-echo " Instalación de dependencias completado "
+echo " Installation of dependencies completed "
 sleep 1
-echo " Creando directorios y disco RAM "
+echo " Creating directories and RAM disk "
 sleep 1
 ################################   Creando directorios y disco RAM    #########################
 
@@ -28,12 +28,12 @@ mkdir /home/sunxi/kernel/zImage
 mkdir /mnt/ramdisk
 mkdir /TableX
 clear
-echo " Directorios creados "
+echo " Directories created "
 ################################   SUNXI-TOOLS    #########################
 cp TableX_defconfig /home/sunxi
 echo " OK "
 sleep 1
-echo " Instalando sunxi-tools"
+echo " Installing sunxi-tools"
 sleep 1
 cd /home/sunxi/tools
 git clone https://github.com/linux-sunxi/sunxi-tools
@@ -41,7 +41,7 @@ cd sunxi-tools
 make -j$(nproc)
 make -j$(nproc) install
 
-echo " Instalación completada"
+echo " Installation completed"
 sleep 1
 ################################   KERNEL LEGACY    #########################
 #echo " Descargando Kernel sunxi"
@@ -49,27 +49,27 @@ sleep 1
 #git clone https://github.com/linux-sunxi/linux-sunxi.git
 
 ################################   CREACIÓN DE IMAGEN ROOTFS    #########################
-echo "      Selección de ubicación de rootfs"
-echo " Elija una opcion para la ubicación de la imagen "
+echo "      Rootfs location selection"
+echo " Choose an option for the locatioin of the image "
 sleep 2
-echo "1. 	RAMdisk (Mas rapido )"
+echo "1. 	RAMdisk (Faster)"
 echo ""
-echo "2. 	En local, mas lento,aconsejado a ordenadores con poca memoria"
-echo "Preparando Imagen Gnu/Linux"
+echo "2. 	Local, slower, advices to computers with low memery"
+echo "Preparing Gnu/Linux Image"
 echo ""
-echo -n "	Seleccione una opcion [1 - 2]"
+echo -n "	Select an option [1 - 2]"
 read imagen
 case $imagen in
 1) mount -t tmpfs -o size=550M tmpfs /mnt/ramdisk && dd if=/dev/zero of=/mnt/ramdisk/trusty.img bs=1 count=0 seek=500M && mkfs.ext4 -b 4096 -F /mnt/ramdisk/trusty.img &&  chmod 777 /mnt/ramdisk/trusty.img && mount -o loop /mnt/ramdisk/trusty.img /TableX;;
 2) dd if=/dev/zero of=/home/sunxi/Imagen/trusty.img bs=1 count=0 seek=500M && mkfs.ext4 -b 4096 -F /home/sunxi/Imagen/trusty.img &&  chmod 777 /home/sunxi/Imagen/trusty.img && mount -o loop /home/sunxi/Imagen/trusty.img /TableX;;
 *) echo "$opc no es una opcion válida.";
-echo "Presiona una tecla para continuar...";
+echo "Press a key to continue ...";
 read foo;;
 esac
 ################################   DEBOOTSTRAP   #########################
 debootstrap --arch=armhf --foreign trusty /TableX
 ################################   SCRIPT DE INICIO DE U-BOOT BOOT.SCR    #########################
-echo " Añadiendo script de inicio "
+echo " Adding start script "
 > /home/sunxi/boot.cmd
 cat <<+ >> /home/sunxi/boot.cmd
 setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p1 rootwait panic=10
@@ -81,17 +81,17 @@ mkimage -C none -A arm -T script -d /home/sunxi/boot.cmd /home/sunxi/boot.scr
 cp /home/sunxi/boot.scr /TableX/boot
 cp /home/sunxi/boot.cmd /TableX/boot
 clear
-echo " Completado"
+echo " Completed"
 sleep 1
 ################################   KERNEL   #########################
-echo " Descargando y descomprimiento Kernel mainline" 
+echo " Downloading and decompressing Kernel mainline" 
 sleep 1
 wget -P /home/sunxi/kernel/mainline https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.17.11.tar.xz
 cd /home/sunxi/kernel/mainline/
 tar -Jxf /home/sunxi/kernel/mainline/linux-4.17.11.tar.xz
 cp /home/sunxi/TableX_defconfig /home/sunxi/kernel/mainline/linux-4.17.11/arch/arm/configs/
 cd /home/sunxi/kernel/mainline/linux-4.17.11
-echo " Compilando "
+echo " Compiling "
 make -j$(nproc) ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf TableX_defconfig
 # sudo make -j$(nproc) ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- xconfig
 make -j$(nproc) ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage modules dtbs 
@@ -101,12 +101,12 @@ cp arch/arm/boot/zImage /TableX/boot/
 cp arch/arm/boot/zImage  /home/sunxi/kernel/zImage
 cd ..
 clear
-echo " Kernel compilado "
+echo " Kernel compiled "
 sleep 1
 ################################   U-BOOT   #########################
-echo " Descarga y compilacion de u-boot "
+echo " Download and compilation of u-boot "
 sleep 1
-echo " Descargando u-boot denx "
+echo " Downloading u-boot denx "
 sleep 1
 cd /home/sunxi/u-boot
 wget ftp://ftp.denx.de/pub/u-boot/u-boot-2017.11.tar.bz2 
@@ -114,25 +114,25 @@ wget ftp://ftp.denx.de/pub/u-boot/u-boot-2018.03.tar.bz2
 cp u-boot-2017.11.tar.bz2 /home/sunxi/u-boot
 tar -xjvf u-boot-2017.11.tar.bz2
 clear
-echo " Descarga y descompresión de u-boot finalizada "
+echo " Download and decompression of finished u-boot "
 sleep 1
-echo " Cuando aparezca el menu "
+echo " When the menu appears "
 sleep 1
-echo " no tiene que configurar nada "
+echo " you do not have to configure anything "
 sleep 1
-echo "para continuar, seleccione Menu ----> File ----> Quit"
+echo "To continue, select Menu ----> File ----> Quit"
 sleep 1
 cd u-boot-2017.11
-echo "      Menu de compilación del u-boot"
-echo " Elija una opción para compilación del u-boot según su modelo de tablet"
+echo "      U-boot compilation menu"
+echo " Choose an option for compiling the u-boot according to your tablet model"
 sleep 2
 echo "1. 	Tablet a13 q8 "
 echo ""
-echo "2. 	Tablet a23 q8 Resolución 800x480"
+echo "2. 	Tablet a23 q8 Resolution 800x480"
 echo ""
-echo "3. 	Tablet a33 q8 Resolución 1024x600"
+echo "3. 	Tablet a33 q8 Resolution 1024x600"
 echo ""
-echo "4. 	Tablet a33 q8 Resolución 800x480"
+echo "4. 	Tablet a33 q8 Resolution 800x480"
 echo ""
 echo "5. 	iNet_3F"
 echo ""
@@ -142,7 +142,7 @@ echo "7. 	iNet_86VS"
 echo ""
 echo "8. 	iNet_D978"
 echo ""
-echo -n "	Seleccione una opcion [1 - 8]"
+echo -n "	Select one opcion [1 - 8]"
 read uboot
 case $uboot in
 1) sudo cp /home/sunxi/kernel/mainline/linux-4.17.11/arch/arm/boot/dts/sun5i-a13-q8-tablet.dtb /TableX/boot &&  make -j$(nproc) ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- q8_a13_tablet_defconfig;;
@@ -154,15 +154,15 @@ case $uboot in
 7) sudo make -j$(nproc) ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- iNet_86VS_defconfig;;
 8) sudo make -j$(nproc) ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- iNet_D978_rev2_defconfig;;
 *) echo "$opc no es una opcion válida.";
-echo "Presiona una tecla para continuar...";
+echo "Press a key to continue ...";
 read foo;;
 esac
 sudo make -j$(nproc) ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-
 sudo cp u-boot-sunxi-with-spl.bin /TableX/boot
 clear
-echo "Compilación de u-boot terminada"
+echo "Compiled u-boot compilation"
 sleep 1
-echo "Iniciando proceso deboostrap"
+echo "Starting deboostrap process"
 sleep 1
 ################################   SCRIPT SEGUNDA PARTE DEBOOSTRAP   #########################
 cp /usr/bin/qemu-arm-static /TableX/usr/bin
@@ -171,7 +171,7 @@ cp /etc/resolv.conf /TableX/etc
 > /home/sunxi/config.sh
 cat <<+ >> /home/sunxi/config.sh
 #!/bin/sh
-echo " Configurando debootstrap segunda fase"
+echo " Configuring debootstrap second phase"
 sleep 3
 /debootstrap/debootstrap --second-stage
 export LANG=C
@@ -180,7 +180,7 @@ echo "deb http://ports.ubuntu.com/ trusty-security main restricted universe mult
 echo "deb http://ports.ubuntu.com/ trusty-updates main restricted universe multiverse" >> /etc/apt/sources.list
 echo "deb http://ports.ubuntu.com/ trusty-backports main restricted universe multiverse" >> /etc/apt/sources.list
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
-echo "Europe/Berlin" > /etc/timezone
+echo "Africa/Johannesburg" > /etc/timezone
 echo "TableX" >> /etc/hostname
 echo "127.0.0.1 TableX localhost
 ::1 ip6-localhost ip6-loopback
@@ -199,11 +199,11 @@ APT::Install-Recommends "0";
 APT::Install-Suggests "0";
 END
 apt-get update
-echo "Reconfigurando parametros locales"
+echo "Reconfiguring local parameters"
 sleep 1
-locale-gen es_ES.UTF-8
-export LC_ALL="es_ES.UTF-8"
-update-locale LC_ALL=es_ES.UTF-8 LANG=es_ES.UTF-8 LC_MESSAGES=POSIX
+locale-gen en_ZA.UTF-8
+export LC_ALL="en_ZA.UTF-8"
+update-locale LC_ALL=en_ZA.UTF-8 LANG=en_ZA.UTF-8 LC_MESSAGES=POSIX
 dpkg-reconfigure locales
 dpkg-reconfigure -f noninteractive tzdata
 apt-get upgrade -y
@@ -217,7 +217,7 @@ addgroup trusty sudo
 +
 chmod +x  /home/sunxi/config.sh
 sudo cp  /home/sunxi/config.sh /TableX/home
-echo "Montando directorios"
+echo "Mounting directories"
 sleep 1
 ################################   MONTAJE DE PARTICIONES   #########################
 sudo mount -o bind /dev /TableX/dev && sudo mount -o bind /dev/pts /TableX/dev/pts && sudo mount -t sysfs sys /TableX/sys && sudo mount -t proc proc /TableX/proc
